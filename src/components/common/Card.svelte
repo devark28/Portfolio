03b1661616ecdee24106c0b5cee93f4f39c05916
devark28 <82113556+devark28@users.noreply.github.com>
2024-06-14 08:17:33 +0300

    UPD: show skills on skillspage

---
+++ src/components/common/Card.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	export let primary: string;
+	export let secondary: string;
+	export let description: string | undefined = undefined;
+</script>
+
+<div class="card">
+	<div class="heading" style="border-bottom-width: {description != undefined ? '1px' : '0px'};">
+		<div class="icon">Icon</div>
+		<div class="title">
+			<div class="primary">{primary}</div>
+			<div class="secondary">{secondary}</div>
+		</div>
+	</div>
+	{#if description != undefined}
+		<div class="description">{description}</div>
+	{/if}
+</div>
+
+<style>
+	.card {
+		display: flex;
+		flex-direction: column;
+		background: #fd7d7d;
+		width: 20rem;
+		height: fit-content;
+		/* max-height: 8rem; */
+		color: #fff;
+		padding: 1rem 2rem;
+		gap: 1rem;
+		border-radius: 0.4rem;
+	}
+
+	.heading {
+		display: flex;
+		border-bottom-width: 0px;
+		border-bottom-style: solid;
+		border-bottom-color: white;
+		padding: 0.4rem 0rem;
+		gap: 0.5rem;
+	}
+
+	.icon {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 4rem;
+		height: 4rem;
+		background: hsla(0, 0%, 100%, 0.2);
+		border-radius: 0.3rem;
+	}
+
+	.title {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.primary {
+		font-size: 2rem;
+	}
+
+	.secondary {
+		font-size: 0.7rem;
+	}
+
+	.description {
+		font-size: 1rem;
+	}
+</style>
--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,18 +1,56 @@
 <script lang="ts">
+	import Card from '../../components/common/Card.svelte';
 	import Viewport from '../../components/common/Viewport.svelte';
+	const mySkills = [
+		{ title: 'HTML', period: '5 years' },
+		{ title: 'CSS', period: '5 years' },
+		{ title: 'Vanilla Js', period: '4 years' },
+		{ title: 'Python', period: '3 years' },
+		{ title: 'ReactJs', period: '3 years' },
+		{ title: 'ExpressJs', period: '3 years' },
+		{ title: 'NestJs', period: '1 years' }
+	];
 </script>
 
 <Viewport>
-	<div class="container">Skills</div>
+	<div class="container">
+		<div class="heading">Skills</div>
+		<div class="cards">
+			{#each mySkills as skill}
+				<Card primary={skill.title} secondary={skill.period} />
+			{/each}
+		</div>
+	</div>
 </Viewport>
 
 <style>
 	.container {
 		display: flex;
 		flex: 1;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		height: 100%;
 		width: 100%;
+	}
+
+	.heading {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		font-size: 1.5rem;
+		padding: 1rem 4rem;
+	}
+
+	.cards {
+		display: flex;
+		flex: 1;
+		flex-flow: wrap;
+		justify-content: center;
+		width: 100%;
+		padding: 2rem 4rem;
+		gap: 1.3rem;
+		overflow: auto;
 	}
 </style>
